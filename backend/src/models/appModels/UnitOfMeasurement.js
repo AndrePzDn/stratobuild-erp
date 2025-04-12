5a868987a158da94747aa9ba7392b7cb2a61bffc
AndrePzDn <andreperezduran@gmail.com>
2025-04-12 15:33:52 -0400

    feat: add resource and unit of measument models

---
+++ backend/src/models/appModels/UnitOfMeasurement.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+  removed: {
+    type: Boolean,
+    default: false,
+  },
+  enabled: {
+    type: Boolean,
+    default: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  symbol: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('UnitOfMeasurement', schema);
