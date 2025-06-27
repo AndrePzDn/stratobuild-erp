438415c6d1eba888e0a198c1cd16f5d61c54490b
AndrePzDn <andreperezduran@gmail.com>
2025-06-27 18:38:28 -0400

    feat: Update docker-compose configuration and add Nginx setup for frontend and backend services

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,17 +1,24 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
-      '/api/ds/query': {
-        target: 'http://localhost:5000',
+      "/api/ds/query": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
+      },
+      "/api/": {
+        target: "http://localhost",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
       },
     },
   },
@@ -20,4 +27,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
